186f6598e4549361530ed8bc2c18aaabb666a926
SimonGideon <simongideon918@gmail.com>
2023-08-22 01:22:07 +0300

    Add user table

---
+++ db/migrate/20230821222119_add_details_to_users.rb
@@ -0,0 +1,8 @@
+class AddDetailsToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :country, :string
+    add_column :users, :user_name, :string
+    add_column :users, :gender, :string
+    add_column :users, :phone_number, :string
+  end
+end
